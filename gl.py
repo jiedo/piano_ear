a93718ba6b8e54830e2f94fd4b98e32ea31e51fb
jiedo <jiedohh@gmail.com>
2016-05-31 16:04:25 +0800

    more gl test

--- gl.py
+++ gl.py
@@ -20,24 +20,48 @@ class Window(pyglet.window.Window):
         self.label = pyglet.text.Label('', font_name='Arial', font_size=18,
             x=10, y=self.height - 10, anchor_x='left', anchor_y='top',
             color=(255, 0, 0, 255))
+        self.set_2d() # 进入2d模式
 
         self.all_sprites = []
+        self.all_vls = []
         self.n = 0
         self.group_bg = pyglet.graphics.OrderedGroup(0)
-        pyglet.clock.schedule(self.update)
-        for i in range(1000):
-            img = pyglet.image.create(random.randint(10, 50),
-                                      random.randint(10, 50),
-                                      pyglet.image.SolidColorImagePattern((100, 100, 100, 255,)))
-            sp = pyglet.sprite.Sprite(img, x = random.randint(100, 600), y=random.randint(100, 600),
-                                      batch=self.batch, group=self.group_bg)
-            self.all_sprites += [sp]
 
+        pyglet.clock.schedule(self.update)
+
+        for i in range(1000):
+            # img = pyglet.image.create(random.randint(10, 50),
+            #                           random.randint(10, 50),
+            #                           pyglet.image.SolidColorImagePattern((100, 100, 100, 255,)))
+            # sp = pyglet.sprite.Sprite(img, x = random.randint(100, 600), y=random.randint(100, 600),
+            #                           batch=self.batch, group=self.group_bg)
+            # self.all_sprites += [sp]
+
+            # vertex_data = tuple([random.randint(100, 600) for i in range(8)])
+            # vl = self.batch.add(4, GL_QUADS, None,
+            #                ('v2i/static', vertex_data), #
+            #                ('c4B', (100, 100, 100, 255) * 4))
+            # self.all_vls += [vl]
+
+            pass
 
     # 每1/60秒调用一次进行更新
     def update(self, dt):
         self.n += 1
         print "n is", self.n, int(dt * 1000)
+
+        time.sleep(0.5)
+        for i in range(2):
+            vertex_data = tuple([random.randint(100, 600) for i in range(8)])
+            vl = pyglet.graphics.vertex_list(4,
+                ('v2i', vertex_data)
+            )
+            glColor3d(0, 0, 1)
+            vl.draw(GL_QUADS)
+
+        pyglet.window.Window.flip(self)
+        print "finished draw"
+        time.sleep(0.5)
 
 
     # 重写Window的on_draw函数
@@ -57,7 +81,6 @@ class Window(pyglet.window.Window):
         # glVertex2i(200, 350)
         # glEnd()
 
-
         # # Draw outlines only
         # glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
 
@@ -67,14 +90,26 @@ class Window(pyglet.window.Window):
         # glVertex2i(75, 100)
         # glVertex2i(200, 200)
         # glEnd()
-        self.clear()
+
+        # self.clear()
+
+        # for i in range(10):
+        #     vertex_data = tuple([random.randint(100, 600) for i in range(8)])
+        #     vl = self.batch.add(4, GL_QUADS, None,
+        #                    ('v2i/static', vertex_data), #
+        #                    ('c4B', (100, 100, 100, 255) * 4))
+        #     self.all_vls += [vl]
+
+
         # self.set_3d() # 进入3d模式
         # glColor3d(0, 1, 0)
 
-        self.set_2d() # 进入2d模式
         self.batch.draw() # 将batch中保存的顶点列表绘制出来
-        self.draw_label() # 绘制label
-        self.draw_reticle() # 绘制窗口中间的十字
+
+
+        print pyglet.clock.get_fps()
+        # self.draw_label() # 绘制label
+        # self.draw_reticle() # 绘制窗口中间的十字
 
 
     # 窗口大小变化响应事件
@@ -101,9 +136,9 @@ class Window(pyglet.window.Window):
         x, y, z = self.position
         text = '%02d (%.2f, %.2f, %.2f)' % (
             pyglet.clock.get_fps(), x, y, z)
-        #print text
+        # print text
         self.label.text = text
-        #print self.label
+        # print self.label
         self.label.draw() # 绘制label的text
 
 
